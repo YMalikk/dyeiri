90a3fabcffe0306892b2a4e74c281ba2db6691ab
samihafsa <hafsa.sami.uds@gmail.com>
2018-03-22 22:34:45 +0100

    Update web.php

--- app/Modules/User/routes/web.php
+++ app/Modules/User/routes/web.php
@@ -4,18 +4,25 @@ Route::group(['module' => 'User', 'middleware' => ['web'], 'namespace' => 'App\M
 
     Route::get('/logout', ['uses' => 'LoginController@logout', 'as' => 'logout']);
     Route::get('/chef_register', 'RegisterController@showChefRegister')->name('showChefRegister');
-    Route::get('/client_register', 'RegisterController@showClientRegister')->name('showClientRegister');
     Route::post('/handleChefRegister', 'RegisterController@handleChefRegister')->name('handleChefRegister');
     Route::post('/handleConnection', 'LoginController@handleConnection')->name('handleConnection');
     Route::get('/user/verify/{token}', 'RegisterController@verifyUser');
 
+    Route::get('/subscriptionProvider/{provider}', 'LoginController@authenticate')->name('authenticate');
+    Route::post('/subscriptionProviderType', 'LoginController@handleSubscriptionProviderType')->name('handleSubscriptionProviderType');
+    Route::get('login/{provider}/callback', 'LoginController@loginProvider')->name('loginProvider')->where(['provider' => 'facebook|google']);
 });
 
 
 Route::group(['module' => 'User','middleware'=>'client', 'namespace' => 'App\Modules\User\Controllers'], function() {
 
-    Route::get('/account', 'UserController@showProfile')->name('showProfile');
+    Route::get('/profile', 'UserController@showProfile')->name('showProfile');
 
 });
 
 
+Route::group(['module'=> 'User','middleware'=>'chef','namespace'=>'App\Modules\User\Controllers'],function () {
+
+    Route::get('/espace', 'UserController@showProfile')->name('showProfile');
+
+});
