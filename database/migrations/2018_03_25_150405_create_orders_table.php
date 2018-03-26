b1b871eed377943676e62e646a09e4576b0a4f14
samihafsa <hafsa.sami.uds@gmail.com>
2018-03-26 01:41:30 +0100

    Create 2018_03_25_150405_create_orders_table.php

---
+++ database/migrations/2018_03_25_150405_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('chef_id')->unsigned();
+            $table->foreign("chef_id")->references('id')->on("chef");
+            $table->integer('user_id')->unsigned();
+            $table->foreign("user_id")->references('id')->on("users");
+            $table->integer('status')->default(0);
+            $table->integer('price');
+            $table->timestamps();
+        });
+
+        Schema::create('food_orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->integer('food_id')->unsigned();
+            $table->foreign('food_id')->references('id')->on('foods');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
