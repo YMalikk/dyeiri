0b30aa40114b346eb23eea4cfba0626bd47fe031
samihafsa <hafsa.sami.uds@gmail.com>
2018-03-26 01:31:04 +0100

    Update web.php

--- app/Modules/Chef/routes/web.php
+++ app/Modules/Chef/routes/web.php
@@ -3,6 +3,8 @@
 Route::group(['module' => 'Chef', 'middleware' => ['web'], 'namespace' => 'App\Modules\Chef\Controllers'], function() {
 
     Route::resource('Chef', 'ChefController');
+    Route::get('/chef/{id}', 'ChefController@showChefProfil')->name('showChefProfil');
+    Route::post('/reviewChef/{id}', 'ChefController@handleChefReview')->name('handleChefReview'); // middleware client
 
 });
 
