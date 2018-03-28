222d7eca8ede9b8e63e803716161f9dc5196f87e
samihafsa <hafsa.sami.uds@gmail.com>
2018-03-28 20:09:06 +0100

    Update web.php

--- app/Modules/Chef/routes/web.php
+++ app/Modules/Chef/routes/web.php
@@ -13,9 +13,13 @@ Route::group(['module'=> 'User','middleware'=>'chef','namespace'=>'App\Modules\C
     Route::get('/me', 'ChefController@showChefProfile')->name('showChefProfile');
     Route::get('/complete', 'ChefController@showChefRegisterStepTwo')->name('showChefRegisterStepTwo');
     Route::post('/complete', 'ChefController@handleCompleteRegisterChef')->name('handleCompleteRegisterChef');
+    Route::get('/confirmOrderChef/{id}', 'ChefController@confirmOrderChef')->name('confirmOrderChef');
+    Route::get('/denyOrderChef/{id}', 'ChefController@denyOrderChef')->name('denyOrderChef');
+    Route::get('/confirmDishReadyChef/{id}', 'ChefController@confirmDishReadyChef')->name('confirmDishReadyChef');
 });
 
 Route::group(['module'=> 'User','middleware'=>'client','namespace'=>'App\Modules\Chef\Controllers'],function () {
 
     Route::post('/reviewChef/{id}', 'ChefController@handleChefReview')->name('handleChefReview');
+    Route::get('/denyOrderClient/{id}', 'ChefController@denyOrderClient')->name('denyOrderClient');
 });
