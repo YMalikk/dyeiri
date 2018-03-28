72831f97378c15e8ae4a5d6d402b86045d2c70ef
samihafsa <hafsa.sami.uds@gmail.com>
2018-03-28 20:14:52 +0100

    Update chefProfile.blade.php

--- app/Modules/Chef/Views/backOffice/chefProfile.blade.php
+++ app/Modules/Chef/Views/backOffice/chefProfile.blade.php
@@ -41,6 +41,7 @@
             <li class="active"><a data-toggle="pill"  href="#menu">Menu</a></li>
             <li><a data-toggle="pill" href="#kicthen" id="kitchen_tab">Cuisine</a></li>
             <li><a data-toggle="pill" href="#setting">Paramétre</a></li>
+            <li><a data-toggle="pill" href="#orders">Mes Commandes</a></li>
         </ul>
     </div>
 </div>
@@ -691,7 +692,169 @@
             <h3>Paramétre </h3>
             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
-</div><!-- End container -->
+    
+    <div id="orders" class="tab-pane fade">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box_style_2" id="main_menu">
+                    <h2 class="inner text-center">Mes Commandes</h2>
+                    <h3 class="nomargin_top" id="starters">Commandes</h3>
+                    <p>
+                        Te ferri iisque aliquando pro, posse nonumes efficiantur in cum. Sensibus reprimique eu pro. Fuisset mentitum deleniti sit ea.
+                    </p>
+                    <div class="panel-group" id="accordion">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" style="padding: 3px 25px!important;">
+                                <a class="accordion-toggle">
+                                    <div class="row" style="display: flex;">
+                                        <div class="col-md-3">
+                                            <h4>Date</h4>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <h4>Client</h4>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <h4>Statut</h4>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <h4>Prix</h4>
+                                        </div>
+                                        <div class="col-md-1" style="flex-wrap: wrap;align-items: center;display: flex;">
+                                            <h4>Détails</h4>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        @foreach($orders as $order)
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse{{$order->id}}">
+                                            <div class="row" style="display: flex;">
+                                                <div class="col-md-3">
+                                                    <h4>{{date("j F Y",strtotime($order->created_at))}}</h4>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <h4>{{$order->client->name}}</h4>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    @if($order->status == 0)
+                                                        <h4><span class="label label-warning">En attente</span></h4>
+                                                    @elseif($order->status == 1)
+                                                        <h4><span class="label" style="background-color: lightseagreen">Confirmé par le chef</span></h4>
+                                                    @elseif($order->status == 2)
+                                                        <h4><span class="label" style="background-color: #415F95">Prêt</span></h4>
+                                                    @elseif($order->status == 3)
+                                                        <h4><span class="label label-info">Confirmé par le livreur</span></h4>
+                                                    @elseif($order->status == 4)
+                                                        <h4><span class="label label-success">Livré</span></h4>
+                                                    @else
+                                                        <h4><span class="label label-danger">Annulé</span></h4>
+                                                    @endif
+                                                </div>
+                                                <div class="col-md-2">
+                                                    <h4>{{$order->price}} DT</h4>
+                                                </div>
+                                                <div class="col-md-1" style="flex-wrap: wrap;align-items: center;display: grid;">
+                                                    <i class="indicator icon_plus_alt2 pull-right"></i>
+                                                </div>
+                                            </div>
+                                        </a>
+                                </div>
+                                <div id="collapse{{$order->id}}" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <table class="table table-responsive cart-list">
+                                            <thead style="text-align: center">
+                                                <tr>
+                                                    <th class="text-center">Image</th>
+                                                    <th class="text-center">Catégorie</th>
+                                                    <th class="text-center">Plat</th>
+                                                    <th class="text-center">Quantité</th>
+                                                    <th class="text-center">Temps de préparation</th>
+                                                    <th class="text-center">Prix Unitaire</th>
+                                                    <th class="text-center">Commentaire du client</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach($order->foods as $food)
+                                                <tr class="text-center">
+                                                    <?php $plat=\App\Modules\Food\Models\Food::find($food->food_id) ?>
+                                                    <td>
+                                                        <img class="thumb_menu_list" style="float: none;" src="{{asset('../storage/img/foods/'.$plat->image)}}" alt="thumb">
+                                                    </td>
+                                                    <td>
+                                                        <h4>{{$plat->category->name}}</h4>
+                                                    </td>
+                                                    <td>
+                                                        <h4>{{$plat->name}}</h4>
+                                                    </td>
+                                                    <td>
+                                                        <h4>{{$food->quantity}}</h4>
+                                                    </td>
+                                                    <td>
+                                                        <h4>{{$plat->preparation_time}} min</h4>
+                                                    </td>
+                                                    <td>
+                                                        <h4>{{$plat->price}} DT</h4>
+                                                    </td>
+                                                    <td>
+                                                       <h4><a href="#" aria-expanded="true" data-toggle="modal" data-target="#foodReview{{$order->id}}{{$plat->id}}"><i class="icon_plus_alt2"></i></a></h4>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
+                                        @if($order->status == 0)
+                                            <button onclick="location.href='{{route('denyOrderChef',['id' => $order->id])}}'" class="btn_2 add_bottom_15 pull-right">Rejeter la commande</button>
+                                            <button onclick="location.href='{{route('confirmOrderChef',['id' => $order->id])}}'" class="btn_1 add_bottom_15 pull-right" style="margin-right: 5px">Confirmer la commande</button>
+                                        @elseif($order->status == 1)
+                                            <button onclick="location.href='{{route('confirmDishReadyChef',['id' => $order->id])}}'" class="btn_1 add_bottom_15 pull-right">Plat prêt</button>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div><!-- End box_style_1 -->
+            </div>
+        </div>
+    </div>
+</div>
+
+@foreach($orders as $order)
+    @foreach($order->foods as $food)
+        <?php $plat=\App\Modules\Food\Models\Food::find($food->food_id) ?>
+        <div class="modal fade" id="foodReview{{$order->id}}{{$plat->id}}" tabindex="-1" role="dialog" aria-labelledby="review" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content modal-popup">
+                    <a href="#" class="close-link"><i class="icon_close_alt2"></i></a>
+                    <form method="post" action="" name="review" style="width: 100%;max-width: 650px" class="popup-form">
+                        <div class="login_icon"><i class="icon_comment_alt"></i></div>
+                        <div class="row">
+                            @if(!$order->food_order_reviews($plat->id))
+                                <h4>Pas de commentaire pour ce plat</h4>
+                            @else
+                                <div class="review_strip_single">
+                                    <?php $client=\App\Modules\User\Models\User::find($order->food_order_reviews($plat->id)->user_id) ?>
+                                    <img src="{{asset('../storage/img/users/avatar/') . '/' . $client->image}}" alt="" class="img-circle" style="width: 80px">
+                                    <small> - {{date("j F Y",strtotime($order->food_order_reviews($plat->id)->created_at))}} -</small>
+                                    <h4 style="text-align: left">{{$client->name}} {{$client->surname}}</h4>
+                                    <p>
+                                        {{$order->food_order_reviews($plat->id)->content}}
+                                    </p>
+                                    <!-- End row -->
+                                </div>
+                            @endif
+                        </div><!--End Row -->
+                        <input href="#" value="Fermer" class="btn btn-submit close-link">
+                    </form>
+                </div>
+            </div>
+</div>
+    @endforeach
+@endforeach
+
+<!-- End container -->
 <!-- End Content =============================================== -->
 <script>
     $('#cat_nav a[href^="#"]').on('click', function (e) {
